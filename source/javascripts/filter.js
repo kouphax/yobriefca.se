b9413268d1ca968c108c9bb02b26119503291d13
James Hughes <james.hughes@digital.cabinet-office.gov.uk>
2013-10-16 19:59:34 +0100

    Added filtering of the main page using some css stuff i recalled on the way home

--- source/javascripts/filter.js
+++ source/javascripts/filter.js
@@ -1,22 +1,28 @@
 (function(global, document) {
 
-	var filterElement = document.createElement("div");
+  var filterElement = document.createElement("div");
 
-	filterElement.setAttribute("class", "filter active")
-	filterElement.innerHTML = [
-		"Filter &rarr;",
-		"<a class='active' data-filter='all' href='#'> All</a> | ",
-		"<a href='#' data-filter='articles'>Articles</a> | ",
-		"<a href='#' data-filter='talks'>Talks</a> | ",
-		"<a href='#' data-filter='screencasts'>Screencasts</a>"].join('');
+  filterElement.setAttribute("class", "filter active")
+  filterElement.innerHTML = [
+    "Filter &rarr; ",
+    "<a class='active' data-filter='all' href='#'>All</a> | ",
+    "<a href='#' data-filter='article'>Articles</a> | ",
+    "<a href='#' data-filter='talk'>Talks</a> | ",
+    "<a href='#' data-filter='screencast'>Screencasts</a>"].join('');
 
-	filterElement.addEventListener('click', function(event) {
+  filterElement.addEventListener('click', function(event) {
+    if(event.target.hasAttribute('data-filter')){
+      document.querySelector('body').setAttribute("class",
+        event.target.getAttribute('data-filter'));
 
-		if(event.target.hasAttribute('data-filter')){
-			alert('delegated')
-		}
-	})
+      Array.prototype.forEach.call(
+        filterElement.querySelectorAll("a"),
+        function(a){ a.setAttribute('class', '') });
 
-	document.getElementById('filter-container').appendChild(filterElement);
+      event.target.setAttribute("class", "active");
+    }
+  })
+
+  document.getElementById('filter-container').appendChild(filterElement);
 
 })(this, document)
