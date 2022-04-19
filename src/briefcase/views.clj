477ca1120ddaee4931a9721bedd1fc0d42162f66
James Hughes <james@yobriefca.se>
2022-04-19 09:32:40 +0100

    Update views.clj

--- src/briefcase/views.clj
+++ src/briefcase/views.clj
@@ -225,11 +225,4 @@
      [:div.row body]
      [:div.homer
        [:a { :href "/" }
-         [:i.icon-briefcase { :style "font-size:32px;" }]]]
-     [:script { :type "text/javascript" } "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-19143623-5', 'yobriefca.se');
-  ga('send', 'pageview');"]]))
+         [:i.icon-briefcase { :style "font-size:32px;" }]]]]))
